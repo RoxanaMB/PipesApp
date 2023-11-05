7fe6378e41f2acef5d24952b7c3ecbf241aac16d
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2023-11-05 23:20:43 +0000

    i18nSelect Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./uncommon-page.component.css']
 })
 export class UncommonPageComponent {
+  // i18n Select
+  public name: string = 'Roxana';
+  public gender: 'male'|'female' = 'female';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Juan';
+    this.gender = 'male';
+  }
 
 }
