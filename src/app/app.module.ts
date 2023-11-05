5b9965d1d5bd3b5528b94cfe6bba3c6c859dc098
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2023-11-05 21:39:30 +0000

    Cambiar idioma local y global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -6,6 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+
+// Configuración del locale de la app
+import localeEs from '@angular/common/locales/es';
+import localeFrCA from '@angular/common/locales/fr-CA';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEs );
+registerLocaleData( localeFrCA );
 
 @NgModule({
   declarations: [
@@ -17,7 +25,10 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    // Configuración del locale de la app
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
